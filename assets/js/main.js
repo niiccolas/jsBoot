de8fa5d1a76b0a62ae0bd317145f2bfe323954f8
niiccolas <40167702+niiccolas@users.noreply.github.com>
2018-09-24 23:41:27 +0200

    Add model object with fire method checking guesses

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ function makeIDs() {
   const board = [];
   for (let i = 0; i < rows.length; i++) {
     for (let j = 0; j < columns.length; j++) {
-      board.push((i + "") + (j))
+      board.push(`${i }${j}`);
     }
   }
   // Tag each cell with a board ID
@@ -24,10 +24,48 @@ const view = {
     const feedback = document.getElementById('displayFeedback');
     feedback.firstElementChild.innerText = msg;
   },
-  displayHit(coord) {
-    document.getElementById(coord).setAttribute('class', 'hit');
+  displayHit(loc) {
+    document.getElementById(loc).setAttribute('class', 'hit');
   },
-  displayMiss(coord) {
-    document.getElementById(coord).setAttribute('class', 'miss');
+  displayMiss(loc) {
+    document.getElementById(loc).setAttribute('class', 'miss');
+  },
+};
+
+// MODEL
+const model = {
+  boardSize: 7,
+  numShips: 3, // ? Redudant with ships.length ?
+  ships: [
+    { positions: ['00', '01', '02'], hits: [] },
+    { positions: ['10', '11', '12'], hits: [] },
+    { positions: ['20', '21', '22'], hits: [] },
+  ],
+  shipsSunk: 0, // if 3 then game is WON
+  shipLength: 3,
+  fire(loc) {
+    for (let i = 0, s = this.ships; i < s.length; i++) {
+      const ship = s[i];
+      const locIndex = ship.positions.indexOf(loc);
+      if (locIndex >= 0) {
+        ship.hits[locIndex] = 'hit';
+        view.displayHit(loc);
+        if (this.isSunk(ship)) {
+          view.displayMessage('BATTLESHIP SUNK');
+          this.shipsSunk++;
+        }
+        return true;
+      }
+    }
+    view.displayMiss(loc);
+    return false;
+  },
+  isSunk(ship) {
+    for (let i = 0; i < this.shipLength; i++) {
+      if (ship.hits[i] !== 'hit') {
+        return false;
+      }
+    }
+    return true;
   },
 };
